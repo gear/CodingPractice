267dde99a2a99e405ba005cffa294193a60809c9
HoangNT <hoangnt@ai.cs.titech.ac.jp>
2016-01-08 11:45:58 +0000

    reverse a list starting from 2nd position

---
+++ ReorderList.java
@@ -0,0 +1,23 @@
+/* ReorderList.java
+ * Reorder a given list: keep the head and reverse order of the rest
+ */
+
+class Node {
+	Node next;
+	int id;
+}
+	
+public class ReorderList {
+	public void reverse(Node root) {
+		if (root == null)
+			return null;
+		if (root.next == null)
+			return null;
+		Node head = root;
+		
+		while(head != null) {
+			
+		}
+		 
+	}
+}
