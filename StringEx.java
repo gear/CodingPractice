8cff0387833cdc841077ff510b7857eeacae7a0f
hoangnt <hoangnt@localhost.localdomain>
2016-02-04 11:06:24 +0900

    String exercise 1

--- StringEx.java
+++ StringEx.java
@@ -1,25 +1,71 @@
 import java.util.HashSet;
+import java.util.HashMap;
 
 public class StringEx {
 
-    /* Assuming we are checking over ASCII character set.
-     * There are 256 characters. */
-    public boolean hasDuplicate(String str) {
-        boolean[] hit = new boolean[256];
-        for (int i = 0; i < str.length(); ++i) {
-            int c = str.charAt(i);
-            if (hit[c]) return false;
-            hit[c] = true;
-        }
-        return true;
-    }
+/* Ex1.1: Implement an algorithm to determine if a string is all unique
+ * using extra data strucuture. Assumption: Only ASCII.
+ 	 * Signature: boolean isAllUniqueASCII(char[] str);
+ 	 */
+	public boolean isAllUniqueASCII(char[] str) {
+		if (str == null)
+			return false;
+		if (str.length < 2)
+			return true;
 
-    /* Assuming we are check over 26 characters of the
-     * English alphabet => We can use bit vector */
-     public boolean hasDuplicateAlphabet(String str) {
-        
-     }
+		boolean[] visited = new boolean[256];
+		for (int i = 0; i < 256; ++i) {
+			visited[i] = false;
+		}
+		for (int i = 0; i < str.length; ++i) {
+			if (visited[i])
+				return false;
+			else
+				visited[i] = true;
+		}
+		return true;
+}
 
+/* Ex1.2: Implement an algorithm to determine if a string is all unique
+ * using extra data structure. Asumption: Any character.
+ * Signature: boolean isAllUnique(char[] str);
+ */
+public boolean isAllUnique(char[] str) {
+	if (str == null)
+			return false;
+		if (str.length < 2)
+			return true;
 
+	HashSet<char> visited = new HashSet<char>();
+	for (int i = 0; i < str.length; ++i) {
+		if (visited.contains(str[i]))
+			return false;
+		else
+			visited.add(str[i]);
+	}
+	return true;
+}
+
+/* Ex1.3: Implement an algorithm ot determine if a string is all unique
+ * without using extra data structure. Assumption: Only from A to Z.
+ * Signature: boolean isAllUniqueAZ(char[] str);
+ */
+public boolean isAllUniqueAZ(char[] str) {
+	if (str == null)
+			return false;
+		if (str.length < 2)
+			return true;
+
+	long visitedMask = 0;
+	for (int i = 0; i < str.length; ++i) {
+		int shift = str[i] - 'A';
+		long checker = 0x1 << var;
+		if (visitedMask & checker > 0)
+			return false;
+		else
+			visitedMask |= checker;
+	} // iterate str
+	return true;
+}
 
 }
